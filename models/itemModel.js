f5be6bb657c6106aa9940f0ebaf252e777d1ab2c
Rmyonathan <yonathan.tanu123@gmail.com>
2025-04-05 02:18:46 +0700

    utils,controller,seeder,model-yoyo

---
+++ models/itemModel.js
@@ -0,0 +1,26 @@
+const { timeStamp } = require('console')
+const mongoose = require('mongoose')
+
+const itemSchema = mongoose.Schema({
+    name :{
+        type:String,
+        required:true 
+    },
+    price :{
+        type:Number,
+        required:true
+    },
+    category :{
+        type:String,
+        required:true
+    },
+    image :{
+        type:String,
+        required:true
+    }
+},{timestamp:true})
+
+
+const Items = mongoose.model("items" ,itemSchema);
+
+module.exports = Items;
--- server.js
+++ server.js
@@ -3,27 +3,32 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const { bgCyan } = require("colors");
 require("colors");
-
-// rest object
+const connectDb = require("./config/config");
+//dotenv config
+dotenv.config();
+//db config
+connectDb();
+//rest object
 const app = express();
 
-// Middlewares
+//middlwares
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan("dev"));
 
-// Routes
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to the POS BACKEND SERVER!</h1>");
-});
+//routes
+app.use("/api/items", require("./routes/itemRoutes"));
 
-// Port
-const POST = process.env.PORT || 8080;
+//port
+const PORT = process.env.PORT || 8080;
 
-// Listen
-app.listen(POST, () => {
-  console.log(`Server is running on port ${POST}`.bgCyan.white);
+
+
+//listen
+app.listen(PORT, () => {
+  console.log(`Server Running On Port ${PORT}`.bgCyan.white);
 });
