f0e49f2a559f90c783ba041286df9b8a5e4a7c35
Rmyonathan <yonathan.tanu123@gmail.com>
2025-04-07 13:18:54 +0700

    add itempage defaultlayout

---
+++ client/src/pages/ItemPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import DefaultLayout from "../components/DefaultLayout";
+
+
+
+const ItemPage = () => {
+  return (
+    <DefaultLayout>
+      <h1>Item List</h1>
+    </DefaultLayout>
+  );
+};
+
+export default ItemPage;
